28518cc3ef74ef1bdbb04c295004a06372d5991c
Annie Tsai <annietsai8@gmail.com>
2026-03-22 09:26:03 +0100

    import room

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import 'reflect-metadata'
+import './App.css';
+import { HologyScene } from '@hology/react'
+import shaders from './shaders'
+import actors from './actors'
+import components from './components';
+import Game from './services/game'
+
+
+function App() {
+  return (
+    <HologyScene gameClass={Game} sceneName='main' dataDir='data' shaders={shaders} actors={actors} components={components}>
+    </HologyScene>
+  );
+}
+
+export default App;
